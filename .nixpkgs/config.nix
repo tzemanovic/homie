6a7f97289cebf39325befff874cc5aa0015c1fbf
Tomas <tzemanovic@gmail.com>
2018-06-30 12:51:43 +1000

    added nix-haskell

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,6 +1,6 @@
 {
   packageOverrides = super: {
-    /* From http://nicknovitski.com/nix-npm-install
+    /* Adapted from http://nicknovitski.com/nix-npm-install
      * Use 'nix-npm-install' to install a npm package. E.g.:
      * 'nix-env -i nix-npm-install' to install this script
      * 'nix-npm-install tern' to install tern
@@ -15,6 +15,27 @@
       ${super.nodePackages.node2nix}/bin/node2nix -8 --input <( echo "[\"$1\"]")
       nix-env --install --file .
       popd
-    ''; 
+    '';
+
+    /* Adapted from https://alpmestan.com/posts/2017-09-06-quick-haskell-hacking-with-nix.html
+     * Use 'nix-haskell' to try a haskell package in nix-shell. E.g.:
+     * 'nix-env -i nix-haskell' to install this script
+     * 'nix-haskell ghc822 protolude' to try protolude package
+     */
+    nix-haskell = super.pkgs.writeScriptBin "nix-haskell" ''
+      #!/usr/bin/env bash
+      if [[ $# -lt 2 ]];
+      then
+        echo "Must provide a ghc version (e.g ghc822) and at least one package"
+        return 1;
+      else
+        ghcver=$1
+        # get the rest of the arguments
+        shift
+        pkgs=$@
+        echo "Starting haskell shell, ghc = $ghcver, pkgs = $pkgs"
+        nix-shell -p "haskell.packages.$ghcver.ghcWithPackages (pkgs: with pkgs; [$pkgs])"
+      fi
+    '';
   };
 }
